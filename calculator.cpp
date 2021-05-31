3dbb69e12c046fec1dcabc2b1376a79e9b99c0b0
Matthew William Reidy <48036844+Matthew-Reidy@users.noreply.github.com>
2021-05-31 15:56:05 -0700

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
 using namespace std;
-int Calc(int i, int x, int y) 
+int Calc(int i, int x, int y)
 {
 	switch (i)
 		case 1:
 			return x + y;
-		case 2;
-			return x - y;
-		case 3:
-			return x / y;
-		case 4: 
-			return x * y;
-}	
+			case 2;
+				return x - y;
+			case 3:
+				return x / y;
+			case 4:
+				return x * y;
+}
 int main() {
 	int i, x, y
-	cout << "please enter a operation you would like to peroform" << endl;
+		cout << "please enter a operation you would like to perform" << endl;
 	cout << "Options are 1: addition 2:subtraction 3:division 4:multiplication" << endl;
 	cin >> i;
-	
+
 	cout << "enter two numbers" << endl;
 	cin >> x >> y;
-	int Calc(i, int x, int y)
+	Calc(i, x, y);
 }
